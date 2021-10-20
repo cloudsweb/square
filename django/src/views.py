eb09c4b5e90f43d3483685b6e88c5cfd06515a92
Clouds Flowing <clouds.flowing@gmail.com>
2021-10-20 16:20:01 +0800

    add views

--- django/src/views.py
+++ django/src/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def index(request: HttpRequest):
+  return HttpResponse("Hello, world. You're at the index.")
+
+def create_user(request: HttpRequest):
+  if request.method != 'POST':
+    return HttpResponse("method not supported", status=405)
+  # TODO: parse request.body
